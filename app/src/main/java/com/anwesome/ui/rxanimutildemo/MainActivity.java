9c4696b1595844b2c7b6d270454facc8463aed50
Anwesh43 <anweshthecool0@gmail.com>
2017-01-19 11:19:01 +0530

    Testing fadout animation when scale animation ends

--- app/src/main/java/com/anwesome/ui/rxanimutildemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/rxanimutildemo/MainActivity.java
@@ -14,12 +14,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ImageView imageView = (ImageView)findViewById(R.id.im_view);
         ImageView imageView2 = (ImageView)findViewById(R.id.im_view2);
-        ImageView imageView3 = (ImageView)findViewById(R.id.im_view3);
+        final ImageView imageView3 = (ImageView)findViewById(R.id.im_view3);
         ImageView imageView4 = (ImageView)findViewById(R.id.im_view4);
-        RxAnimUtil.rotateView(imageView,40,270,100);
-        RxAnimUtil.translateView(imageView2,300,700, RxAnimUtil.RxAnimTranslation.XY,100);
-        RxAnimUtil.scaleView(imageView3,0.2f,1.5f,100);
-        RxAnimUtil.fadeInView(imageView4,300);
+        RxAnimUtil.rotateView(imageView,40,270,100,null);
+        RxAnimUtil.translateView(imageView2,300,700, RxAnimUtil.RxAnimTranslation.XY,100,null);
+        RxAnimUtil.scaleView(imageView3, 0.2f, 1.5f, 100, new RxAnimUtil.RxAnimationListener() {
+            @Override
+            public void onAnimationEnd() {
+                RxAnimUtil.fadeOutView(imageView3,300,null);
+            }
+
+            @Override
+            public void onAnimationRunning() {
+
+            }
+
+            @Override
+            public void onAnimationStart() {
+
+            }
+        });
+        RxAnimUtil.fadeInView(imageView4,300,null);
     }
     public void onPause() {
         super.onPause();
